bdcc2ad91a8086f3d5561a5fb51f9b55effd72c5
chimex53 <chimex53@gmail.com>
2024-08-24 13:03:44 +0100

    Event handling

--- my-app/src/Events/Input.jsx
+++ my-app/src/Events/Input.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Input = ()=>{
-    return(
+    const [HeadingText,setHeadingText]=useState("hello");
+    const [inputVal,setInputVal]=useState("");
+
+
+function click(){
+ setHeadingText("submitted")
+} 
+
+  return(
         <div className="container">
-            <h1>hello</h1>
-            <input type="text" placeholder="username"/>
-            <button>Submit</button>
+            <h1>{HeadingText}</h1>
+            <input
+            //ref={inputRef}
+            onChange={(e) => setInputVal(e.target.value)}
+            type="text" placeholder="username" 
+            value={inputVal}
+            />
+            <button onClick={click}>Submit</button>
         </div>
     );
 }
