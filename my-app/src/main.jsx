725e80800843e0c785e154cd3c96fd8a05fcbdd1
chimex53 <chimex53@gmail.com>
2024-08-26 17:36:23 +0100

    todo list

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import React, {useState} from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import React, { useState } from "react";
+
 /* import Input from './Events/Input'; */
 /* import ComplexState from './ComplexState/ComplexState'; */
 
-import Todo from './todolist/Todo';
+import Todo from "./todolist/Todo";
 
 /* import Practice from './object/Practice.jsx'; */
 /* import './sytling.css' */
@@ -13,11 +14,11 @@ import Apps from '../form2/Apps'
 import Ap from '../state/Ap' */
 /* import Index from '../hooks' */
 /* import UseStatePrac from './useState/UseStatePrac'; */
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-{/*   <UseStatePrac /> */}
-{/* <Practice/> */}
-{/* <Input /> */}
-<Todo/>
-  </StrictMode>,
-)
+    {/*   <UseStatePrac /> */}
+    {/* <Practice/> */}
+    {/* <Input /> */}
+    <Todo />
+  </StrictMode>
+);
--- my-app/src/todolist/Todo.jsx
+++ my-app/src/todolist/Todo.jsx
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import "./todo.css";
 
-function Todo(){
+function Todo() {
+  const [inputText, setInputText] = useState("");
+  const [Items, setItems] = useState([]);
 
+  function handleChange(e) {
+    const newValue = e.target.value;
+    setInputText(newValue);
+  }
 
+  function addItem() {
+    setItems((previousitem) => {
+      return [...previousitem, inputText];
+    });
+    setInputText("");
+  }
 
-return(
-    <div>
+  return (
+    <div className="container">
+      <div className="heading">
         <h1>Todo-list</h1>
-        <input type="text" value={} onChange={} />
-        <button>Add</button>
+      </div>
+      <div className="form">
+        <input
+          type="text"
+          value={inputText}
+          onChange={handleChange}
+          placeholder="Add list"
+        />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <ul>
+        {Items.map((todoItem) => {
+          return <li>{todoItem}</li>;
+        })}
+      </ul>
     </div>
-)
+  );
 }
+
 export default Todo;
